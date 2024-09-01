1e0d63c54e37fe968ad48912aa1a416f588337dd
NathanGros <nathan.gros6@gmail.com>
2024-09-01 19:25:34 +0200

    renamed almost everything for clean code

--- game.c
+++ game.c
@@ -27,9 +27,9 @@ typedef struct {
 	int y1;
 	int x2;
 	int y2;
-	int destroom;
-	int destx;
-	int desty;
+	int destRoom;
+	int destX;
+	int destY;
 } Gate;
 
 typedef struct {
@@ -37,16 +37,16 @@ typedef struct {
 	int y1;
 	int x2;
 	int y2;
-	int blocknb;
-	int wallnb;
-	int gatenb;
+	int blockNb;
+	int wallNb;
+	int gateNb;
 	Block **blocks; //pointer on a list of pointers
 	Wall **walls;
 	Gate **gates;
 } Room;
 
 typedef struct {
-	int roomnb; //number of rooms
+	int roomNb; //number of rooms
 	Room **rooms;
 } Map;
 
@@ -56,6 +56,7 @@ typedef struct {
 } Force;
 
 typedef struct {
+	Color color;
 	int radius;
 	float mass;
 	float airRes;
@@ -71,61 +72,62 @@ typedef struct {
 
 //Init
 
-void Init(Color bg) {
+void Init(Color backgroundColor) {
 	InitWindow(1500, 900, "Blob game");
-	ClearBackground(bg);
+	ClearBackground(backgroundColor);
 	SetTargetFPS(60);
 }
 
 Block* InitBlock(int x1, int y1, int x2, int y2, int type) {
-	Block *b = malloc(sizeof(Block));
-	b->x1 = x1;
-	b->y1 = y1;
-	b->x2 = x2;
-	b->y2 = y2;
-	b->type = type;
-	return b;
+	Block *block = malloc(sizeof(Block));
+	block->x1 = x1;
+	block->y1 = y1;
+	block->x2 = x2;
+	block->y2 = y2;
+	block->type = type;
+	return block;
 }
 
 Wall* InitWall(int x1, int y1, int x2, int y2, char direction) {
-	Wall *w = malloc(sizeof(Wall));
-	w->x1 = x1;
-	w->y1 = y1;
-	w->x2 = x2;
-	w->y2 = y2;
-	w->direction = direction;
-	return w;
+	Wall *wall = malloc(sizeof(Wall));
+	wall->x1 = x1;
+	wall->y1 = y1;
+	wall->x2 = x2;
+	wall->y2 = y2;
+	wall->direction = direction;
+	return wall;
 }
 
-Gate* InitGate(int x1, int y1, int x2, int y2, int destroom, int destx, int desty) {
-	Gate *g = malloc(sizeof(Gate));
-	g->x1 = x1;
-	g->y1 = y1;
-	g->x2 = x2;
-	g->y2 = y2;
-	g->destroom = destroom;
-	g->destx = destx;
-	g->desty = desty;
-	return g;
+Gate* InitGate(int x1, int y1, int x2, int y2, int destRoom, int destX, int destY) {
+	Gate *gate = malloc(sizeof(Gate));
+	gate->x1 = x1;
+	gate->y1 = y1;
+	gate->x2 = x2;
+	gate->y2 = y2;
+	gate->destRoom = destRoom;
+	gate->destX = destX;
+	gate->destY = destY;
+	return gate;
 }
 
-Room* InitRoom(int blocknb, int wallnb, int gatenb, int x1, int y1, int x2, int y2) {
-	Room* r = malloc(sizeof(Room));
-	r->x1 = x1;
-	r->y1 = y1;
-	r->x2 = x2;
-	r->y2 = y2;
-	r->blocks = malloc(blocknb * sizeof(Block));
-	r->blocknb = blocknb;
-	r->walls = malloc(wallnb * sizeof(Wall));
-	r->wallnb = wallnb;
-	r->gates = malloc(gatenb * sizeof(Gate));
-	r->gatenb = gatenb;
-	return r;
+Room* InitRoom(int blockNb, int wallNb, int gateNb, int x1, int y1, int x2, int y2) {
+	Room* room = malloc(sizeof(Room));
+	room->x1 = x1;
+	room->y1 = y1;
+	room->x2 = x2;
+	room->y2 = y2;
+	room->blocks = malloc(blockNb * sizeof(Block));
+	room->blockNb = blockNb;
+	room->walls = malloc(wallNb * sizeof(Wall));
+	room->wallNb = wallNb;
+	room->gates = malloc(gateNb * sizeof(Gate));
+	room->gateNb = gateNb;
+	return room;
 }
 
-Body* InitBody(int radius, float mass,	float airRes, int posX,	int posY, int velocityX, int velocityY, int forceNb) {
+Body* InitBody(Color color, int radius, float mass, float airRes, int posX, int posY, int velocityX, int velocityY, int forceNb) {
 	Body *body = malloc(sizeof(Body));
+	body->color = color;
 	body->radius = radius;
 	body->mass = mass;
 	body->airRes = airRes;
@@ -142,55 +144,50 @@ Body* InitBody(int radius, float mass,	float airRes, int posX,	int posY, int vel
 
 //Draw
 
-void DrawBlock(Block *b, int w, int h, int camx, int camy, float zoom, Color col) {
+void DrawBlock(Block *block, int screenWidth, int screenHeight, int cameraX, int cameraY, float zoom, Color blockColor) {
 	DrawRectangle(
-		(w/2 - (int) (zoom * (float) (camx - b->x1))),
-		(h/2 - (int) (zoom * (float) (camy - b->y1))),
-		(int) (zoom * (float) (b->x2 - b->x1)),
-		(int) (zoom * (float) (b->y2 - b->y1)),
-		col
+		(screenWidth / 2 - (int) (zoom * (float) (cameraX - block->x1))),
+		(screenHeight / 2 - (int) (zoom * (float) (cameraY - block->y1))),
+		(int) (zoom * (float) (block->x2 - block->x1)),
+		(int) (zoom * (float) (block->y2 - block->y1)),
+		blockColor
 	);
 }
 
-void DrawRoom(Room *r, int w, int h, int camx, int camy, float zoom, Color bg_col, Color block_col) {
+void DrawRoom(Room *room, int screenWidth, int screenHeight, int cameraX, int cameraY, float zoom, Color backgroundColor, Color blockColor) {
 	DrawRectangle(
-		(w/2 - (int) (zoom * (float) (camx - r->x1))),
-		(h/2 - (int) (zoom * (float) (camy - r->y1))),
-		(int) (zoom * (float) (r->x2 - r->x1)),
-		(int) (zoom * (float) (r->y2 - r->y1)),
-		bg_col
+		(screenWidth / 2 - (int) (zoom * (float) (cameraX - room->x1))),
+		(screenHeight / 2 - (int) (zoom * (float) (cameraY - room->y1))),
+		(int) (zoom * (float) (room->x2 - room->x1)),
+		(int) (zoom * (float) (room->y2 - room->y1)),
+		backgroundColor
 	);
-	for (int i = 0; i < r->blocknb; i++) {
-		Block *b = r->blocks[i];
-		Color draw_col;
-		if (b->type == 0)
-			draw_col = block_col;
-		else
-			draw_col = bg_col;
-		DrawBlock(b, w, h, camx, camy, zoom, draw_col);
+	for (int i = 0; i < room->blockNb; i++) {
+		Block *block = room->blocks[i];
+		DrawBlock(block, screenWidth, screenHeight, cameraX, cameraY, zoom, blockColor);
 	}
 }
 
-void DrawWalls(Room *r, int w, int h, int camx, int camy, float zoom) {
-	for (int i = 0; i < r->wallnb; i++) {
-		Wall *wall = r->walls[i];
+void DrawWalls(Room *room, int screenWidth, int screenHeight, int cameraX, int cameraY, float zoom) {
+	for (int i = 0; i < room->wallNb; i++) {
+		Wall *wall = room->walls[i];
 		DrawLine(
-			(w/2 - (int) (zoom * (float) (camx - wall->x1))),
-			(h/2 - (int) (zoom * (float) (camy - wall->y1))),
-			(w/2 - (int) (zoom * (float) (camx - wall->x2))),
-			(h/2 - (int) (zoom * (float) (camy - wall->y2))),
+			(screenWidth / 2 - (int) (zoom * (float) (cameraX - wall->x1))),
+			(screenHeight / 2 - (int) (zoom * (float) (cameraY - wall->y1))),
+			(screenWidth / 2 - (int) (zoom * (float) (cameraX - wall->x2))),
+			(screenHeight / 2 - (int) (zoom * (float) (cameraY - wall->y2))),
 			RED
 		);
 	}
 }
 
-void DrawPlayer(int playerx, int playery, int playerradius, int w, int h, int camx, int camy, float zoom) {
+void DrawPlayer(Body *player, int screenWidth, int screenHeight, int cameraX, int cameraY, float zoom) {
 	DrawRectangle(
-		(w/2 - (int) (zoom * (float) (camx - (playerx-playerradius)))),
-		(h/2 - (int) (zoom * (float) (camy - (playery-playerradius)))),
-		(int) (zoom * (float) (2 * playerradius)),
-		(int) (zoom * (float) (2 * playerradius)),
-		(Color) {115, 0, 255, 255}
+		(screenWidth / 2 - (int) (zoom * (float) (cameraX - (player->posX - player->radius)))),
+		(screenHeight / 2 - (int) (zoom * (float) (cameraY - (player->posY - player->radius)))),
+		(int) (zoom * (float) (2 * player->radius)),
+		(int) (zoom * (float) (2 * player->radius)),
+		player->color
 	);
 }
 
@@ -198,32 +195,40 @@ void DrawPlayer(int playerx, int playery, int playerradius, int w, int h, int ca
 
 //Functions
 
-bool IsAboveLine(int coeff, int pointx, int pointy, int x, int y) {
-	int ordinateAtOrigin = pointy - coeff * pointx; //Line : t |--> coeff * t + ordinateAtOrigin
-	return y > (coeff * x + ordinateAtOrigin);
+bool IsAboveLine(int slope, int pointOnLineX, int pointOnLineY, int posX, int posY) {
+	int ordinateAtOrigin = pointOnLineY - slope * pointOnLineX; //Linear function : t |--> slope * t + ordinateAtOrigin
+	return (posY > (slope * posX + ordinateAtOrigin));
 }
 
-bool CheckCollisionPlayerWall(int newx, int newy, int x, int y, int playerradius, int x1, int y1, int x2, int y2, char direction) {
-	bool inwall = false;
-	if (direction == 'U' && newy + playerradius > y1 && newy - playerradius < y1 && newx + playerradius > x1 && newx - playerradius < x2
-		&& !IsAboveLine(1, x1, y1, x, y) && !IsAboveLine(-1, x2, y2, x, y)) inwall = true;
-	if (direction == 'D' && newy + playerradius > y1 && newy - playerradius < y1 && newx + playerradius > x1 && newx - playerradius < x2
-		&& IsAboveLine(-1, x1, y1, x, y) && IsAboveLine(1, x2, y2, x, y)) inwall = true;
-	if (direction == 'L' && newx + playerradius > x1 && newx - playerradius < x1 && newy + playerradius > y1 && newy - playerradius < y2
-		&& IsAboveLine(1, x1, y1, x, y) && !IsAboveLine(-1, x2, y2, x, y)) inwall = true;
-	if (direction == 'R' && newx + playerradius > x1 && newx - playerradius < x1 && newy + playerradius > y1 && newy - playerradius < y2
-		&& IsAboveLine(-1, x1, y1, x, y) && !IsAboveLine(1, x2, y2, x, y)) inwall = true;
-	return inwall;
+bool CheckCollisionPlayerWall(int newPosX, int newPosY, Body *player, int x1, int y1, int x2, int y2, char direction) {
+	bool isInWall = false;
+	if (direction == 'U'
+		&& newPosY + player->radius > y1 && newPosY - player->radius < y1
+		&& newPosX + player->radius > x1 && newPosX - player->radius < x2
+		&& !IsAboveLine(1, x1, y1, player->posX, player->posY) && !IsAboveLine(-1, x2, y2, player->posX, player->posY)) isInWall = true;
+	if (direction == 'D'
+		&& newPosY + player->radius > y1 && newPosY - player->radius < y1
+		&& newPosX + player->radius > x1 && newPosX - player->radius < x2
+		&& IsAboveLine(-1, x1, y1, player->posX, player->posY) && IsAboveLine(1, x2, y2, player->posX, player->posY)) isInWall = true;
+	if (direction == 'L'
+		&& newPosX + player->radius > x1 && newPosX - player->radius < x1
+		&& newPosY + player->radius > y1 && newPosY - player->radius < y2
+		&& IsAboveLine(1, x1, y1, player->posX, player->posY) && !IsAboveLine(-1, x2, y2, player->posX, player->posY)) isInWall = true;
+	if (direction == 'R'
+		&& newPosX + player->radius > x1 && newPosX - player->radius < x1
+		&& newPosY + player->radius > y1 && newPosY - player->radius < y2
+		&& IsAboveLine(-1, x1, y1, player->posX, player->posY) && !IsAboveLine(1, x2, y2, player->posX, player->posY)) isInWall = true;
+	return isInWall;
 }
 
-void ExecuteCollisions(Room *r, int *newx, int *newy, int x, int y, int playerradius) {
-	for (int i = 0; i < r->wallnb; i++) {
-		Wall *wall = r->walls[i];
-		if (CheckCollisionPlayerWall(*newx, *newy, x, y, playerradius, wall->x1, wall->y1, wall->x2, wall->y2, wall->direction)) {
-			if (wall->direction == 'U') *newy = wall->y1 - playerradius;
-			if (wall->direction == 'D') *newy = wall->y1 + playerradius;
-			if (wall->direction == 'L') *newx = wall->x1 - playerradius;
-			if (wall->direction == 'R') *newx = wall->x1 + playerradius;
+void ExecuteCollisions(Room *room, int *newPosX, int *newPosY, Body *player) {
+	for (int i = 0; i < room->wallNb; i++) {
+		Wall *wall = room->walls[i];
+		if (CheckCollisionPlayerWall(*newPosX, *newPosY, player, wall->x1, wall->y1, wall->x2, wall->y2, wall->direction)) {
+			if (wall->direction == 'U') *newPosY = wall->y1 - player->radius;
+			if (wall->direction == 'D') *newPosY = wall->y1 + player->radius;
+			if (wall->direction == 'L') *newPosX = wall->x1 - player->radius;
+			if (wall->direction == 'R') *newPosX = wall->x1 + player->radius;
 		}
 	}
 }
@@ -234,130 +239,130 @@ void ExecuteCollisions(Room *r, int *newx, int *newy, int x, int y, int playerra
 
 void main() {
 	//Init
-	Color bg_col = (Color) {200, 200, 200, 255};
-	Color block_col = (Color) {20, 20, 20, 255};
+	Color backgroundColor = (Color) {200, 200, 200, 255};
+	Color blockColor = (Color) {20, 20, 20, 255};
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-	Init(block_col);
+	Init(blockColor);
 	float zoom = 1.;
-	int w = GetScreenWidth();
-	int h = GetScreenHeight();
-	int camx = 0;
-	int camy = 0;
-	bool showwalls = false;
-	Body *player = InitBody(20, 1., 1., 0, -100, 0, 0, 1);
+	int screenWidth = GetScreenWidth();
+	int screenHeight = GetScreenHeight();
+	int cameraX = 0;
+	int cameraY = 0;
+	bool displayWalls = false;
+	Body *player = InitBody((Color) {115, 0, 255, 255}, 20, 1., 1., 0, -100, 0, 0, 1);
 
 	//Make the map
 		//blocks
-		Block *b0_r0 = InitBlock(-50, 0, 50, 300, 0);
-		Block *b0_r1 = InitBlock(0, -500, 400, -400, 0);
+		Block *block0_room0 = InitBlock(-50, 0, 50, 300, 0);
+		Block *block0_room1 = InitBlock(0, -500, 400, -400, 0);
 		
 		//gates
-		Gate *g0_r0 = InitGate(-600, 200, -500, 300, 1, 350, -550);
-		Gate *g0_r1 = InitGate(400, -600, 500, -500, 0, -450, 250);
+		Gate *gate0_room0 = InitGate(-600, 200, -500, 300, 1, 350, -550);
+		Gate *gate0_room1 = InitGate(400, -600, 500, -500, 0, -450, 250);
 
 		//rooms
-		Room *r0 = InitRoom(1, 8, 1, -500, -300, 500, 300);
-		r0->blocks[0] = b0_r0;
-		r0->gates[0] = g0_r0;
-		r0->walls[0] = InitWall(-500, -300, 500, -300, 'D');
-		r0->walls[1] = InitWall(500, -300, 500, 300, 'L');
-		r0->walls[2] = InitWall(-500, -300, -500, 200, 'R');
-		r0->walls[3] = InitWall(-500, 300, -50, 300, 'U');
-		r0->walls[4] = InitWall(50, 300, 500, 300, 'U');
-		r0->walls[5] = InitWall(-50, 0, 50, 0, 'U');
-		r0->walls[6] = InitWall(-50, 0, -50, 300, 'L');
-		r0->walls[7] = InitWall(50, 0, 50, 300, 'R');
+		Room *room0 = InitRoom(1, 8, 1, -500, -300, 500, 300);
+		room0->blocks[0] = block0_room0;
+		room0->gates[0] = gate0_room0;
+		room0->walls[0] = InitWall(-500, -300, 500, -300, 'D');
+		room0->walls[1] = InitWall(500, -300, 500, 300, 'L');
+		room0->walls[2] = InitWall(-500, -300, -500, 200, 'R');
+		room0->walls[3] = InitWall(-500, 300, -50, 300, 'U');
+		room0->walls[4] = InitWall(50, 300, 500, 300, 'U');
+		room0->walls[5] = InitWall(-50, 0, 50, 0, 'U');
+		room0->walls[6] = InitWall(-50, 0, -50, 300, 'L');
+		room0->walls[7] = InitWall(50, 0, 50, 300, 'R');
 
-		Room *r1 = InitRoom(1, 7, 1, -400, -600, 400, 600);
-		r1->blocks[0] = b0_r1;
-		r1->gates[0] = g0_r1;
-		r1->walls[0] = InitWall(-400, -600, 400, -600, 'D');
-		r1->walls[1] = InitWall(-400, -600, -400, 600, 'R');
-		r1->walls[2] = InitWall(-400, 600, 400, 600, 'U');
-		r1->walls[3] = InitWall(0, -500, 0, -400, 'L');
-		r1->walls[4] = InitWall(0, -400, 400, -400, 'D');
-		r1->walls[5] = InitWall(0, -500, 400, -500, 'U');
-		r1->walls[6] = InitWall(400, -400, 400, 600, 'L');
+		Room *room1 = InitRoom(1, 7, 1, -400, -600, 400, 600);
+		room1->blocks[0] = block0_room1;
+		room1->gates[0] = gate0_room1;
+		room1->walls[0] = InitWall(-400, -600, 400, -600, 'D');
+		room1->walls[1] = InitWall(-400, -600, -400, 600, 'R');
+		room1->walls[2] = InitWall(-400, 600, 400, 600, 'U');
+		room1->walls[3] = InitWall(0, -500, 0, -400, 'L');
+		room1->walls[4] = InitWall(0, -400, 400, -400, 'D');
+		room1->walls[5] = InitWall(0, -500, 400, -500, 'U');
+		room1->walls[6] = InitWall(400, -400, 400, 600, 'L');
 
 		//map
-		Map *m = malloc(sizeof(Map));
-		m->rooms = malloc(2 * sizeof(Room));
-		m->roomnb = 2;
-		m->rooms[0] = r0;
-		m->rooms[1] = r1;
+		Map *map = malloc(sizeof(Map));
+		map->rooms = malloc(2 * sizeof(Room));
+		map->roomNb = 2;
+		map->rooms[0] = room0;
+		map->rooms[1] = room1;
 	
-	Room *current_room = r0;
+	Room *currentRoom = room0;
 
 	while (!WindowShouldClose()) {
 	//Resize
 		if (IsWindowResized()) {
-			w = GetScreenWidth();
-			h = GetScreenHeight();
+			screenWidth = GetScreenWidth();
+			screenHeight = GetScreenHeight();
 		}
 
 	//Controls
-		int newplayerx = player->posX;
-		int newplayery = player->posY;
+		int newPosX = player->posX;
+		int newPosY = player->posY;
 		if (IsKeyPressed(KEY_I) && zoom < 2.) zoom += 0.1; 
 		if (IsKeyPressed(KEY_O) && zoom > 0.5) zoom -= 0.1; 
-		if (IsKeyDown(KEY_W)) newplayery -= 10; 
-		if (IsKeyDown(KEY_A)) newplayerx -= 10; 
-		if (IsKeyDown(KEY_S)) newplayery += 10; 
-		if (IsKeyDown(KEY_D)) newplayerx += 10; 
-		if (IsKeyPressed(KEY_R)) showwalls = !showwalls; 
+		if (IsKeyDown(KEY_W)) newPosY -= 10; 
+		if (IsKeyDown(KEY_A)) newPosX -= 10; 
+		if (IsKeyDown(KEY_S)) newPosY += 10; 
+		if (IsKeyDown(KEY_D)) newPosX += 10; 
+		if (IsKeyPressed(KEY_R)) displayWalls = !displayWalls; 
 
 	//Wall collisions
-		ExecuteCollisions(current_room, &newplayerx, &newplayery, player->posX, player->posY, player->radius);
-		player->posX = newplayerx;
-		player->posY = newplayery;
+		ExecuteCollisions(currentRoom, &newPosX, &newPosY, player);
+		player->posX = newPosX;
+		player->posY = newPosY;
 
 	//Camera update
-		camx = player->posX;
-		camy = player->posY;
+		cameraX = player->posX;
+		cameraY = player->posY;
 	
-	//Changing room
-		Room *new_room = current_room;
-		for (int i = 0; i < current_room->gatenb; i++) {
-			Gate *g = current_room->gates[i];
-			Rectangle gate = (Rectangle) {g->x1, g->y1, g->x2 - g->x1, g->y2 - g->y1};
-			Rectangle playerrect = (Rectangle) {player->posX - player->radius, player->posY - player->radius, 2 * player->radius, 2 * player->radius};
-			if (CheckCollisionRecs(gate, playerrect)) {
-				new_room = m->rooms[g->destroom];
-				player->posX = g->destx;
-				player->posY = g->desty;
+	//Changing room TODO
+		Room *newRoom = currentRoom;
+		for (int i = 0; i < currentRoom->gateNb; i++) {
+			Gate *gate = currentRoom->gates[i];
+			Rectangle gateRect = (Rectangle) {gate->x1, gate->y1, gate->x2 - gate->x1, gate->y2 - gate->y1};
+			Rectangle playerRect = (Rectangle) {player->posX - player->radius, player->posY - player->radius, 2 * player->radius, 2 * player->radius};
+			if (CheckCollisionRecs(gateRect, playerRect)) {
+				newRoom = map->rooms[gate->destRoom];
+				player->posX = gate->destX;
+				player->posY = gate->destY;
 			}
 		}
-		current_room = new_room;
+		currentRoom = newRoom;
 		
 	//Drawing
 		BeginDrawing();
-			ClearBackground(block_col);
-			DrawRoom(current_room, w, h, camx, camy, zoom, bg_col, block_col);
-			if (showwalls) DrawWalls(current_room, w, h, camx, camy, zoom);
-			DrawPlayer(player->posX, player->posY, player->radius, w, h, camx, camy, zoom);
+			ClearBackground(blockColor);
+			DrawRoom(currentRoom, screenWidth, screenHeight, cameraX, cameraY, zoom, backgroundColor, blockColor);
+			if (displayWalls) DrawWalls(currentRoom, screenWidth, screenHeight, cameraX, cameraY, zoom);
+			DrawPlayer(player, screenWidth, screenHeight, cameraX, cameraY, zoom);
 		EndDrawing();
 	}
 	CloseWindow();
 
 	//De-init
-	for(int i = 0; i < m->roomnb; i++) {
-		Room *r = m->rooms[i];
-		for(int j = 0; j < r->blocknb; j++) {
-			free(r->blocks[j]);
+	for(int i = 0; i < map->roomNb; i++) {
+		Room *room = map->rooms[i];
+		for(int j = 0; j < room->blockNb; j++) {
+			free(room->blocks[j]);
 		}
-		free(r->blocks);
-		for(int j = 0; j < r->wallnb; j++) {
-			free(r->walls[j]);
+		free(room->blocks);
+		for(int j = 0; j < room->wallNb; j++) {
+			free(room->walls[j]);
 		}
-		free(r->walls);
-		for(int j = 0; j < r->gatenb; j++) {
-			free(r->gates[j]);
+		free(room->walls);
+		for(int j = 0; j < room->gateNb; j++) {
+			free(room->gates[j]);
 		}
-		free(r->gates);
-		free(r);
+		free(room->gates);
+		free(room);
 	}
-	free(m->rooms);
-	free(m);
+	free(map->rooms);
+	free(map);
 	free(player->forces);
 	free(player);
 }
